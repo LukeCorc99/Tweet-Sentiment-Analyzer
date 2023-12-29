1f0f5879d5dc9867b8b15d39d13612f52e1d03b9
g00410404 <145585794+g00410404@users.noreply.github.com>
2023-12-29 22:49:19 +0000

    lexicons fully complete

--- Lexicons.java
+++ Lexicons.java
@@ -10,47 +10,47 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class Lexicons {
-	// Declare a static variable to keep track of the line number
-	private static int line = 0;
+    // Declare a static variable to keep track of the line number
+    private static int line = 0;
 
-	// Declare a ConcurrentSkipListMap to store words and their corresponding scores
-	private ConcurrentSkipListMap<String, String> lexWords = new ConcurrentSkipListMap<>();
+    // Declare a ConcurrentSkipListMap to store words and their corresponding scores
+    private ConcurrentSkipListMap<String, Double> lexWords = new ConcurrentSkipListMap<>();
 
-	// Method to process a lexicon file
-	public void go(String lexicon) throws Exception {
-		// Construct the path to the lexicon file
-		String path = "./Lexicons/" + lexicon;
+    // Method to process a lexicon file
+    public void go(String lexicon) throws Exception {
+        // Construct the path to the lexicon file
+        String path = "./Lexicons/" + lexicon;
 
-		// Create a new virtual thread executor
-		try (var pool = Executors.newVirtualThreadPerTaskExecutor()) {
-			// Read each line of the file and process it in a separate thread
-			Files.lines(Paths.get(path)).forEach(text -> pool.execute(() -> process(text, ++line)));
-		} catch (IOException e) {
-			// Handle any IOExceptions that might be thrown when reading the file
-			e.printStackTrace();
-		} catch (Exception e) {
-			// Handle any other exceptions that might be thrown when processing the text
-			e.printStackTrace();
-		}
+        // Create a new virtual thread executor
+        try (var pool = Executors.newVirtualThreadPerTaskExecutor()) {
+            // Read each line of the file and process it in a separate thread
+            Files.lines(Paths.get(path)).forEach(text -> pool.execute(() -> process(text, ++line)));
+        } catch (IOException e) {
+            // Handle any IOExceptions that might be thrown when reading the file
+            e.printStackTrace();
+        } catch (Exception e) {
+            // Handle any other exceptions that might be thrown when processing the text
+            e.printStackTrace();
+        }
 
-		// Print the total number of words
-		out.println("\nLexicon successfully processed! Number of words is: "+lexWords.size());
-	}
+        // Print the total number of words
+        System.out.println("\nLexicon successfully processed! Number of words is: "+lexWords.size());
+    }
 
-	// Method to process a line of text
-	public void process(String text, int line) {
-		// Split the line into a word and a score
-		String[] parts = text.split(",");
+    // Method to process a line of text
+    public void process(String text, double line) {
+        // Split the line into a word and a score
+        String[] parts = text.split(",");
 
-		// If the line contains exactly two parts
-		if (parts.length == 2) {
-			// The first part is the word
-			String word = parts[0];
-			// The second part is the score
-			String score = parts[1];
+        // If the line contains exactly two parts
+        if (parts.length == 2) {
+            // The first part is the word
+            String word = parts[0];
+            // The second part is the score
+            Double score = Double.parseDouble(parts[1]);
 
-			// Store the word and score in the ConcurrentSkipListMap
-			lexWords.put(word, score);
-		}
-	}
+            // Store the word and score in the ConcurrentSkipListMap
+            lexWords.put(word, score);
+        }
+    }
 }
